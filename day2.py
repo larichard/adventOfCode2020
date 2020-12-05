44b3f37e2a30a688e09de0012ddcbfbd8c9307bc
rla-trinity <rla@trinity.edu>
2020-12-04 22:03:29 -0900

    day 3 + fixed day 2

--- day2.py
+++ day2.py
@@ -13,6 +13,8 @@ def validByNum(policy):
         char = x[1][0]
         # list of times char must be in password 
         nums = list(map(int, x[0].split("-")))
+        lo = nums[0]
+        hi = nums[1]
         # the password
         psw = x[2]
 
@@ -20,19 +22,41 @@ def validByNum(policy):
             if(element == char):
                 times += 1 
 
-        #print (nums[1])
-        if (times >= nums[0] and times <= nums[1]):
+        if (times >= lo and times <= hi):
             valid += 1
 
     print(valid)
 
 def validByPosition(policy):
     valid = 0
+    for x in policy:
+        # if char is in correct position
+        times = False
+        # char required
+        char = x[1][0]
+        # list of locations char must be in password
+        nums = list(map(int, x[0].split("-")))
+        posLo = nums[0]
+        posHi = nums[1]
+        # the password
+        psw = x[2]
+
+        if ((psw[posLo-1] == char and psw[posHi-1] != char) or 
+            (psw[posLo-1] != char and psw[posHi-1] == char)):
+            times = True
+        
+        if times:
+            valid += 1
+    
+    print(valid)
+
 
 
 def main():
     print("Part 1:")
     validByNum(splitfile)
+    print("Part 2:")
+    validByPosition(splitfile)
 
 if __name__ == "__main__":
     main()
